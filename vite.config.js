d8dee5f363a82264959f87844677a494f16aaec5
wsw-dev-hub <wsw.contato@gmail.com>
2026-06-18 03:12:27 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -97,8 +97,9 @@ export default defineConfig(({ command }) => ({
         'posts/9-licoes': resolve(__dirname, 'src/posts/9-licoes.html'),
         //'posts/post-2': resolve(__dirname, 'src/posts/post-2.html'),
       },
+       external: ['ton-connect', 'ton-core'],
      output: {
-        external: ['ton-connect', 'ton-core'],
+       
         /*manualChunks: {
           'ton-connect': ['@tonconnect/ui', '@tonconnect/sdk'],
           'ton-core': ['@ton/ton', '@ton/core', '@ton/crypto'],
