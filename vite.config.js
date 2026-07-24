a5c294a504a005cd5d00ff70feedcbaf91bb41ed
wsw-dev-hub <wsw.contato@gmail.com>
2026-07-24 17:48:15 -0300

    update: fix url sessions

--- vite.config.js
+++ vite.config.js
@@ -111,8 +111,9 @@ export default defineConfig(({ command }) => ({
         'apps/apps': resolve(__dirname, 'src/apps/apps.html'),
         //'posts/post-2': resolve(__dirname, 'src/posts/post-2.html'),
         'estagio/talentos': resolve(__dirname, 'public/membros/estagio/arvore-de-talentos/index.html'),
+        'membros/temporada-free': resolve(__dirname, 'public/membros/temporada-free/index.html'),
+
         'membros/capacitacao': resolve(__dirname, 'public/membros/capacitacao/estagiario.html'),
-        'membros/temporada-free': resolve(__dirname, 'public/membros/fundamentos/temporada-free.html'),
         
       },
       external: ['ton-connect', 'ton-core'],
