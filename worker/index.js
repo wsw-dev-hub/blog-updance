563d353c769b0ea46ad9b30cf8c5b0b05918b14e
wsw-dev-hub <wsw.contato@gmail.com>
2026-08-01 00:01:53 -0300

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -27,7 +27,7 @@
  */
 
 import { WorkerMailer } from 'worker-mailer';
-import { produtosList, produtosSave, produtosDelete, produtosVinculosSave } from 'worker_produtos.js';
+import { produtosList, produtosSave, produtosDelete, produtosVinculosSave } from 'worker_produtos';
 
 const SESSION_TTL  = 60 * 60 * 24 * 7;
 const MAGIC_TTL    = 60 * 15;
